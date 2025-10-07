e60a57f0a46ba81ca0daaa75eab49bac820977aa
raihan-ms <raihan@mavosoft.com>
2025-10-07 15:39:36 +0600

    feat: Update component exports for branded and post-purchase survey methods

--- src/components/survey-builder/distribution/methods/branded-survey/components/index.ts
+++ src/components/survey-builder/distribution/methods/branded-survey/components/index.ts
@@ -1,4 +1,4 @@
-export { default as SimplifiedCustomization } from "./SimplifiedCustomization";
 export { default as LogoSettings } from "./LogoSettings";
-export { default as SurveyUrlSection } from "./SurveyUrlSection";
+export { default as SimplifiedCustomization } from "./SimplifiedCustomization";
 export { default as SurveyContentSection } from "./SurveyContentSection";
+export { default as SurveyUrlSection } from "./SurveyUrlSection";
--- src/components/survey-builder/distribution/methods/post-purchase/components/index.ts
+++ src/components/survey-builder/distribution/methods/post-purchase/components/index.ts
@@ -1,2 +1,3 @@
 export { default as PageLocationSettings } from "./PageLocationSettings";
+export { default as SideLogo } from "./SideLogo";
 export { default as UserTargeting } from "./UserTargeting";
